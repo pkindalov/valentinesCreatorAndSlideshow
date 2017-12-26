a221999742137a65866240520f180d38afbe0ab3
pkindalov <panayotkindalov@gmail.com>
2017-12-27 00:31:29 +0200

    Removed more unnecessairy files

--- backup/pics.js
+++
@@ -1,9 +0,0 @@
-function getPics(){ return [ 
-'pictures/gift.jpg',
-'pictures/gift2.jpg',
-'pictures/gift3.jpg',
-'pictures/gift4.jpg',
-'pictures/gift5.jpg',
-'pictures/gift6.jpg',
-'pictures/gift7.jpg',
-]} 
